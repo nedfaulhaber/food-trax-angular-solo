e7b982aa9e7aa84974ca5300b637914c73eed80f
Amanda Daly and Alex Hagge <alexhagge128@gmail.com>
2016-05-31 10:29:30 -0700

    building angular structures

--- app/app.component.ts
+++ app/app.component.ts
@@ -6,8 +6,29 @@ import { Component } from 'angular2/core';
     <div class="container">
       <h1>Skeleton Angular2 App!</h1>
     </div>
+
+    <div *ngFor="#task of tasks">
+      <h3>{{ task.description }}</h3>
+      <h4> Hi, I'm another nested HTML element. There are 4 copies of me because we have 4 tasks.</h4>
+    </div>
   `
 })
-export class AppComponent {
 
+export class AppComponent {
+  public tasks: Task[];
+  constructor(){
+    this.tasks = [
+      new Task("Create To-Do List app.", 0),
+      new Task("Learn Kung Fu.", 1),
+      new Task("Rewatch all the Lord of the Rings movies.", 2),
+      new Task("Do the laundry.", 3)
+    ]
+  }
+}
+
+export class Task {
+  public done: boolean = false;
+  constructor(public description: string, public id: number) {
+
+  }
 }
