b596454556697d2022b535ba5d636b4858faa607
Alex Hagge and Amanda Daly <alexhagge128@gmail.com>
2016-05-31 15:35:32 -0700

    added emitter functionality

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,45 +1,59 @@
 import { Component, EventEmitter } from 'angular2/core';
 
 
-///-------------------------------------------
+//--------Child of TaskListComponent---------
+@Component({
+    selector: 'task-display',
+    inputs: ['task'],
+  template: `
+    <h3>{{ task.description }}</h3>
+  `
+})
+export class TaskComponent {
+  public task: Task;
+}
+//-----Child of AddComponent----------
 @Component({
   selector: 'task-list',
   inputs: ['taskList'],
   outputs: ['onTaskSelect'],
+  directives: [TaskComponent],
   template: `
-  <h3 *ngFor="#currentTask of taskList" (click)="taskClicked(currentTask)">
+  <h3 *ngFor="#currentTask of taskList"
+    (click)="taskClicked(currentTask)"
+    [class.selected]="currentTask === selectedTask">
     {{ currentTask.description }}
   </h3>
   `
 })
-
 export class TaskListComponent {
-
   public taskList: Task[];
   public onTaskSelect: EventEmitter<Task>;
+  public selectedTask: Task;
   constructor() {
     this.onTaskSelect = new EventEmitter();
   }
   taskClicked(clickedTask: Task): void {
     console.log('child', clickedTask);
+    this.selectedTask = clickedTask;
     this.onTaskSelect.emit(clickedTask);
   }
 }
 
-///-------------------------------------------
+//-------Parent-----------
 @Component({
   selector: 'my-app',
+  directives: [TaskListComponent],
   template: `
-
     <div class="container">
-        <h1>To-Do List</h1>
-        <h3 *ngFor="#task of tasks" (click)="taskWasSelected(task)">
-          {{ task.description }}
-        </h3>
+      <h1>To-Do List</h1>
+      <task-list
+        [taskList]="tasks"
+        (onTaskSelect)="taskWasSelected($event)">
+      </task-list>
     </div>
   `
 })
-
 export class AppComponent {
   public tasks: Task[];
   constructor(){
@@ -48,13 +62,11 @@ export class AppComponent {
       new Task("Learn Kung Fu.", 1),
       new Task("Rewatch all the Lord of the Rings movies.", 2),
       new Task("Do the laundry.", 3)
-    ]
+    ];
   }
-
   taskWasSelected(clickedTask: Task): void {
-      console.log(clickedTask);
-    }
-
+    console.log('parent', clickedTask);
+  }
 }
 
 export class Task {
