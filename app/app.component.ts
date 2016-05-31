ceded41c366ef9c5285ff17c1e2d4baa7f24eaa3
Alex Hagge and Amanda Daly <alexhagge128@gmail.com>
2016-05-31 11:59:44 -0700

    added child component

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,15 +1,41 @@
-import { Component } from 'angular2/core';
+import { Component, EventEmitter } from 'angular2/core';
 
+
+///-------------------------------------------
+@Component({
+  selector: 'task-list',
+  inputs: ['taskList'],
+  outputs: ['onTaskSelect'],
+  template: `
+  <h3 *ngFor="#currentTask of taskList" (click)="taskClicked(currentTask)">
+    {{ currentTask.description }}
+  </h3>
+  `
+})
+
+export class TaskListComponent {
+
+  public taskList: Task[];
+  public onTaskSelect: EventEmitter<Task>;
+  constructor() {
+    this.onTaskSelect = new EventEmitter();
+  }
+  taskClicked(clickedTask: Task): void {
+    console.log('child', clickedTask);
+    this.onTaskSelect.emit(clickedTask);
+  }
+}
+
+///-------------------------------------------
 @Component({
   selector: 'my-app',
   template: `
-    <div class="container">
-      <h1>Skeleton Angular2 App!</h1>
-    </div>
 
-    <div *ngFor="#task of tasks">
-      <h3>{{ task.description }}</h3>
-      <h4> Hi, I'm another nested HTML element. There are 4 copies of me because we have 4 tasks.</h4>
+    <div class="container">
+        <h1>To-Do List</h1>
+        <h3 *ngFor="#task of tasks" (click)="taskWasSelected(task)">
+          {{ task.description }}
+        </h3>
     </div>
   `
 })
@@ -24,6 +50,11 @@ export class AppComponent {
       new Task("Do the laundry.", 3)
     ]
   }
+
+  taskWasSelected(clickedTask: Task): void {
+      console.log(clickedTask);
+    }
+
 }
 
 export class Task {
