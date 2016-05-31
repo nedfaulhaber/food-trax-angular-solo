bea54ec39d78453ba37eba600ed953b6f557c4f0
Alex Hagge and Amanda Daly <alexhagge128@gmail.com>
2016-05-31 16:29:36 -0700

    separated templates

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,46 +1,7 @@
 import { Component, EventEmitter } from 'angular2/core';
+import { TaskListComponent } from './task-list.component';
+import { Task } from './task.model';
 
-
-//--------Child of TaskListComponent---------
-@Component({
-    selector: 'task-display',
-    inputs: ['task'],
-  template: `
-    <h3>{{ task.description }}</h3>
-  `
-})
-export class TaskComponent {
-  public task: Task;
-}
-//-----Child of AddComponent----------
-@Component({
-  selector: 'task-list',
-  inputs: ['taskList'],
-  outputs: ['onTaskSelect'],
-  directives: [TaskComponent],
-  template: `
-  <h3 *ngFor="#currentTask of taskList"
-    (click)="taskClicked(currentTask)"
-    [class.selected]="currentTask === selectedTask">
-    {{ currentTask.description }}
-  </h3>
-  `
-})
-export class TaskListComponent {
-  public taskList: Task[];
-  public onTaskSelect: EventEmitter<Task>;
-  public selectedTask: Task;
-  constructor() {
-    this.onTaskSelect = new EventEmitter();
-  }
-  taskClicked(clickedTask: Task): void {
-    console.log('child', clickedTask);
-    this.selectedTask = clickedTask;
-    this.onTaskSelect.emit(clickedTask);
-  }
-}
-
-//-------Parent-----------
 @Component({
   selector: 'my-app',
   directives: [TaskListComponent],
@@ -66,12 +27,5 @@ export class AppComponent {
   }
   taskWasSelected(clickedTask: Task): void {
     console.log('parent', clickedTask);
-  }
-}
-
-export class Task {
-  public done: boolean = false;
-  constructor(public description: string, public id: number) {
-
   }
 }
